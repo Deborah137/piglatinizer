023745ee7e8648984f861214027024bb6cf08795
Deborah kwong <meneverland29@gmail.com>
2019-11-06 23:25:59 +0000

    comment goes here

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$( "document" ).ready(function() {
+    $("button").click(function(){
+        $(".output").text("");
+        let sentances =$("input").val();
+        let wording = sentences.split(' ');
+        let vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+        wording.forEach(function(word){
+            let firstLetter=word[0].toLowerCase();
+            if (vowels.includes(firstLetter)) {
+                console.log(true);
+                let vowelWord= word + "ay ";
+                $('.output').append(vowelWord);
+            }else {
+                  console.log(false);
+                let conWord= word.slice(1) + firstLetter + "ay ";
+                $('.output').append(conWord);
 
+            }
+        });
 
+    });
 
-  
-  
-  
-  
 });
+
+
